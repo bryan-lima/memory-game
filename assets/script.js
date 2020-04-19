f6178280d4697f7b0e88a034829d35c4e58002bc
Bryan <bryanlima200@outlook.com>
2020-04-18 23:12:07 -0300

    Iniciado criação do conteúdo do arquivo script.js

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+/**************************************************************************************
+*
+* Desenvolvido por BRYAN LIMA
+* GitHub: https://github.com/bryan-lima
+*
+* Atividade Acadêmica - Jogo da Memória
+* Análise e Desenvolvimento de Sistemas - ADS
+* Repositório deste projeto: https://github.com/bryan-lima/memory-game
+*
+**************************************************************************************/
+
